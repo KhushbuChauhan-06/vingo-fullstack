c1c77c830516b2925c409aaf66d6805bf1003923
KhushbuChauhan-06 <khushbuchauhan062002@gmail.com>
2026-02-15 16:13:03 +0530

    secure cors for production

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin: [
-    "http://localhost:5173",
-    "https://your-frontend-url.onrender.com"
-  ],
+      origin: "https://vingo-fullstack-1.onrender.com",
 
     credentials:true,
     methods:['POST','GET']
